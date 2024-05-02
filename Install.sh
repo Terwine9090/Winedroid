89e62ce6b320e5b12fbc37dc51a69919739bf503
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-05-02 16:53:24 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -16,7 +16,9 @@ cd ubuntu/opt
 tar -xf wine-9.3.tar.gz
 mv wine-9.3 wine
 cd
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> .bashrc
 echo "termux-x11 &> /dev/null & bash Winedroid.sh" >> $PREFIX/bin/winedroid
 chmod +x $PREFIX/bin/winedroid
 clear
 echo "start the winedroid type winedroid and enter"
+source .bashrc
