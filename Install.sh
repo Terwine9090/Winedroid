5cea2449b03cb4b7c963946dd5882ba22ac96efe
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-05-02 14:52:03 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,3 +6,7 @@ apt install proot wget -y &> /dev/null
 apt install x11-repo -y &> /dev/null
 apt install termux-x11-nightly -y &> /dev/null
 apt install virglrenderer-android -y &> /dev/null
+apt install pulseaudio -y &> /dev/null
+wget https://github.com/Terwine9090/Winedroid/releases/download/1.0.0/rootfs.tar.xz
+tar -xf rootfs.tar.xz
+wget
