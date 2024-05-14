2c35b22c4e10a8ccc1d315da6fe07b9da214e00f
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-05-14 17:36:22 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,25 +1,23 @@
 echo "click allow"
 termux-setup-storage
-echo "run apt update"
+echo "instaling package"
 apt update &> /dev/null
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null 2>&1
-
-echo "instaling package"
 apt install proot wget -y &> /dev/null
 apt install x11-repo -y &> /dev/null
 apt install termux-x11-nightly -y &> /dev/null
 apt install virglrenderer-android -y &> /dev/null
 apt install pulseaudio -y &> /dev/null
+echo "downloading rootfs"
 wget https://github.com/Terwine9090/Winedroid/releases/download/1.0.0/rootfs.tar.xz
+echo "extract rootfs"
 tar -xf rootfs.tar.xz
 wget https://raw.githubusercontent.com/Terwine9090/Winedroid/main/Winedroid.sh
-cd ubuntu/opt
-tar -xf wine-9.3.tar.gz
-mv wine-9.3 wine
 cd
 echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> .bashrc
 echo "termux-x11 &> /dev/null & bash Winedroid.sh" >> $PREFIX/bin/winedroid
 chmod +x $PREFIX/bin/winedroid
+winedroid
 clear
 echo "start the winedroid type winedroid and enter"
 source .bashrc
