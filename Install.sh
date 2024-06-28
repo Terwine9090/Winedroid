74ae5fc25d300b3eab34d73f1452773a98120b22
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-06-28 08:23:32 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,11 +9,11 @@ apt install x11-repo -y &> /dev/null
 apt install termux-x11-nightly -y &> /dev/null
 apt install virglrenderer-android -y &> /dev/null
 apt install pulseaudio -y &> /dev/null
-
+mkdir -p $PREFIX/glibc
+cd $PREFIX/glibc
+wget https://raw.githubusercontent.com/Terwine9090/Winedroid/main/sync-package &> /dev/null
+source sync-package
 echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> .bashrc
-echo "termux-x11 &> /dev/null & bash Winedroid.sh" >> $PREFIX/bin/winedroid
-chmod +x $PREFIX/bin/winedroid
-winedroid
 clear
 echo "start the winedroid type winedroid and enter"
 source .bashrc
