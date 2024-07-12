42d3694d337094a986a3d966d25ef6f3035aada1
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-07-12 19:23:08 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -19,4 +19,4 @@ chmod +x $PREFIX/bin/winedroid
 echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> .bashrc
 clear
 echo "start the winedroid type winedroid and enter"
-source .bashrc
+
